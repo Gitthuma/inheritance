b3cd802564f542f9f1a285ef03de08dd436d38b3
Gitthuma <george.githuma@gmail.com>
2021-11-20 22:58:06 +0300

    inherit student class and print its properties

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 # import student class from new
 from new import Student
+
+# inherit student class and print its properties
+
+
+class Person(Student):
+    pass
+
+
+print(Person.name, Person.age, Person.gender)
